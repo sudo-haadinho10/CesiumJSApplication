6c4d008c769f68245a812ebd4b2a3b764a9942f5
sudo-haadinho10 <haadimwork@outlook.com>
2025-05-16 16:35:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,4 @@
 
-// import { defineConfig } from 'vite';
-// import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-// const cesiumSource = 'node_modules/cesium/Build/Cesium';
-// const cesiumBaseUrl = 'cesiumStatic';
-
-// export default defineConfig({
-//   define: {
-//     CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
-//   },
-//   plugins: [
-//     viteStaticCopy({
-//       targets: [
-//         {
-//           src: `${cesiumSource}/*`,
-//           dest: cesiumBaseUrl,
-//         },
-//         {
-//           src: 'GDALOUTPUT', // Copy the entire GDALOUTPUT folder, including subdirectories
-//           dest: 'tiles',
-//         },
-//       ],
-//     }),
-//   ],
-//   server: {
-//     port: 5173,
-//     cors: true,
-//   },
-// });
-
-//abv is best
-
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
@@ -48,7 +16,10 @@ export default defineConfig({
           src: `${cesiumSource}/*`,
           dest: cesiumBaseUrl,
         },
-        // Removed the GDALOUTPUT copying since tiles are served from WSL
+        {
+          src: 'GDALOUTPUT', // Copy the entire GDALOUTPUT folder, including subdirectories
+          dest: 'tiles',
+        },
       ],
     }),
   ],
